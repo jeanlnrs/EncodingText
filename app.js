18fbfc20f71693dcd19f7bf3607d73bf612da757
Jean Linares <jean.lnrs@outlook.com>
2020-04-02 22:20:55 -0500

    Se agregan comentarios del codigo

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
 function CuadroDeTexto() {
-    //Ingresamos los mensajes a mostrar
+    //Ingresamos el mensajes del texto
     var texto = prompt("¿Cuál es el texto que deseas encriptar?", "");
     //Detectamos si el usuario ingreso un valor
     if (texto != "" && texto != null) {
+    //Preguntamos el lenguaje del encriptamiento
     var codigo = parseInt(prompt("Escriba el numero del lenguaje al que desea encriptar su texto \n 1= Base64  2= ROT13  3= Hexadecimal  4= Binario", ""),0);
         switch (codigo) {
             case 1:
+                //En caso de seleccionar la opcion 1 llama a una funcion
                 var encodedString = btoa(texto);
                 alert("El texto " + texto + " en BASE64 es '" + encodedString + "'");
                 break;
 
             case 2:
+                //En caso de seleccionar la opcion 2 llama a una funcion proporcionada de internet, se almacena en una variable para ser luego manejado en la impresion
                 var encodedString = texto.replace(/[a-zA-Z]/g, function (c) { return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26); });
                 alert("El texto " + texto + " en ROT13 es '" + encodedString + "'");
                 break;
 
             case 3:
+                //En caso de seleccionar la opcion 3 llama a una funcion proporcionada de internet
                 var encodedString = ascii_to_hexa(texto);
                 alert("El texto " + texto + " en HEXADECIMAL es '" + encodedString + "'");
                 break;
 
             case 4:
+                //En caso de seleccionar la opcion 4 llama a una funcion proporcionada de internet
                 var encodedString = textToBin(texto);
                 alert("El texto " + texto + " en BINARIO es \n'" + encodedString + "'");
                 break;
 
             default:
+                //Se agrega mensaje de error en caso que el usuario no haya ingresado ninguna de las opciones esperadas
                 alert("El numero dijitado no es valido");
                 break;
         }
@@ -36,6 +42,7 @@ function CuadroDeTexto() {
     }
 }
 
+//Funcion porporcionada de internet que nos retorna una cadena convertida en hexadecimales
 function ascii_to_hexa(str) {
     var arr1 = [];
     for (var n = 0, l = str.length; n < l; n++) {
@@ -45,25 +52,7 @@ function ascii_to_hexa(str) {
     return arr1.join('');
 }
 
-function rot5(str) {
-    var s = [];
-    for (i = 0; i < str.length; i++) {
-        idx = str.charCodeAt(i);
-        if ((idx >= 48) && (idx <= 57)) {
-            if (idx <= 52) {
-                s[i] = String.fromCharCode(((idx + 5)));
-            }
-            else {
-                s[i] = String.fromCharCode(((idx - 5)));
-            }
-        }
-        else {
-            s[i] = String.fromCharCode(idx);
-        }
-    }
-    return s.join('');
-}
-
+//Funcion porporcionada de internet que nos retorna una cadena convertida en binario
 function textToBin(text) {
     return (
         Array
